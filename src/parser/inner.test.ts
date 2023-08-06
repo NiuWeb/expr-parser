fbf852a0e28af3adc24dfd2261cd830a77f4843c
NiuWeb <niuweb1channel@gmail.com>
2023-08-06 18:17:49 -0500

    remove evaluation test from inner grouping does not contain arithmetic evaluation. This should be tested after hydration

--- src/parser/inner.test.ts
+++ src/parser/inner.test.ts
@@ -1,7 +1,6 @@
 import { scan } from "@src/scanner"
-import { clearList, groupOperators } from "./inner"
+import { clearList } from "./inner"
 import { wrapTokens } from "./wrap"
-import { stringFromNodes } from "./string"
 
 
 describe("invalid commas are not allowed", () => {
@@ -27,33 +26,4 @@ test("valid commas are allowed", () => {
     clearList(nodes)
 
     expect(nodes.length).toBe(4)
-})
-
-describe("+ and - can be unary", () => { 
-    test("1 + -2", () => {
-        const tokens = scan("1 + -2")
-        const nodes = wrapTokens(tokens)
-        groupOperators(nodes)
-
-        const root = nodes[0]
-        expect(root.evaluate!()).toBe(-1)
-    })
-    test("-1", () => { 
-        const tokens = scan("-1")
-        const nodes = wrapTokens(tokens)
-        groupOperators(nodes)
-
-        const root = nodes[0]
-        expect(root.evaluate!()).toBe(-1)
-    })
-})
-
-test("Group nodes by operators", () => {
-    const nodes = wrapTokens(scan("1+2*3-4/5^6 - 1 + 1"))
-    groupOperators(nodes)
-
-    const root = nodes[0]
-    expect(root.evaluate!()).toBeCloseTo(1 + 2 * 3 - 4 / 5 ** 6 - 1 + 1)
-
-    console.log(stringFromNodes(nodes))
 })
