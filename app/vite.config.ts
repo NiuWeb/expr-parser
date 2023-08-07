4c70ed13d5d301662000d01c9da0f35305b551e4
NiuWeb <niuweb1channel@gmail.com>
2023-08-06 22:19:46 -0500

    update deploy settings

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import monacoEditorPlugin from "vite-plugin-monaco-editor"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/expr-parser/",
   plugins: [
     react(),
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
